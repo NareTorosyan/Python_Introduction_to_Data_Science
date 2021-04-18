a440de532c1abbc0c2e2d5863238cec9b993e757
toshibal50\Telman <haykk.yeghiazaryan@gmail.com>
2021-04-19 01:23:51 +0400

    test

---
+++ src/first_month/task_1_1_1.py
@@ -0,0 +1,23 @@
+#1Given two whole numbers - the lengths of the legs of a right-angled triangle - output its area.
+a = 3
+b = 4
+s = (a*b)/2 # to get the area of the right angle triangle we multiply the values of angles and divide by two
+print(s)
+
+#2Input a natural number n and output its last digit.
+n = 569
+n=str(n)
+last_digit = int(n[len(n)-1])
+print(last_digit)
+
+#3Input a two-digit natural number and output the sum of its digits.
+n = 65
+m = 6+5
+m
+
+#4 You are given the first and second number in an arithmetic progression and natural number  n. Find n-th element of arithmetic progression.
+a= 5
+b = 10
+n = 3
+c = a + (n-1)*(b-a)
+c
