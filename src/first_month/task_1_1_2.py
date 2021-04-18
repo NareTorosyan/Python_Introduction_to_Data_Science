b5f36c090af4cb7aa18dd858c7ff3b05d8c3da80
toshibal50\Telman <haykk.yeghiazaryan@gmail.com>
2021-04-19 01:39:52 +0400

    test

---
+++ src/first_month/task_1_1_2.py
@@ -0,0 +1,49 @@
+
+
+#1Write a Python program to get a string made of the first 2 and the last 2 chars from a given string.
+s = "Spring"
+s[:2] + s[-1]+s[-2]
+
+#2Write a Python program to remove the nth index character from a nonempty string.
+
+s = "Spring"
+n=3
+s1=s[0:n]+s[n+1:]
+s1
+
+#3Write a Python program to change a given string to a new string where the first and last chars have been exchanged.
+s = "Spring"
+s2 = s[-1] + s[1:5] + s[0]
+s2
+
+
+#4Write a Python function to get a string made of 4 copies of the last two characters of a specified string
+s = "Spring"
+s3 = s[-2:]*4
+s3
+
+
+#1Write a Python program that make a list from given string
+s = "Spring"
+s4 = [s[0],s[1],s[2],s[3],s[4],s[5]]
+s4
+
+
+#2Write a Python program to print a new list which is the equivalent given list after removing the 0th, 4th and 5th elements.
+x = [1,2,3,4,5,6,7,8]
+x.pop(0)
+x.pop(3)
+x.pop(4)
+x
+
+
+#3Write a Python program which add an element to the given list
+x = [1,2,3,4,5,6,7,8]
+x.insert(3,5)
+x
+
+
+#4Write a Python program which concat 2 lists.
+s1 = ["winter","spring"]
+s2 = ["summer", "fall"]
+s1+s2
