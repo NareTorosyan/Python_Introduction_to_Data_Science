bc52f7a5d22e6bdb3d60904f1e0d880ec3ed75c2
toshibal50\Telman <haykk.yeghiazaryan@gmail.com>
2021-04-18 21:13:19 +0400

    test

---
+++ src/first_month/task_1_1_1.py
@@ -0,0 +1,7 @@
+#1 Write a Python program to get the largest number from a list.
+x = [1,2,3,4,5,6,7,8,9]
+max(x)
+
+#2 Write a Python program to get the frequency of the given element in a list to.
+x =[1,2,3,4,5,6,7,8,9,9,9,9,9]
+x.count(9)
