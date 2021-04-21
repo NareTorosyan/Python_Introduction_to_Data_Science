1cdfa1ab055348a16e97efbcd830074e04e711ee
Nare Torosyan <torosyan.nare@gmail.com>
2021-04-21 08:14:12 +0400

    test

---
+++ src/first_month/task_1_3_1.py
@@ -0,0 +1,17 @@
+
+
+#1 Write a Python function, which gets 2 numbers, and return True if the second number is first number divider, otherwise False.
+x = 54
+y = 9
+if x % y == 0:
+    print("True")
+else:
+    print("False")
+#2 Write a Python function, which gets a number, and return True if that number is palindrome, otherwise False
+x = 16461
+n=5
+s = str(x)
+if s[0:n]== s[-n:]:
+    print("True")
+else:
+    print("False")
