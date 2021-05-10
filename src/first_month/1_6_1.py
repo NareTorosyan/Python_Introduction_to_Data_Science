caf5f0d1caaa611dede5a6cb935227874742e7a7
Nare Torosyan <torosyan.nare@gmail.com>
2021-05-10 20:45:08 +0400

    test

--- src/first_month/1_6_1.py
+++ src/first_month/1_6_1.py
@@ -1,17 +1,45 @@
-from task_1_3_1 import gcd
+class RationalException(Exception):
+    def __init__(self, message, value):
+        self.__message = message
+        self.__value = value
+
+    def print_obj(self):
+        print(self.__message, self.__value)
 class Rational:
     def __init__(self,numerator,denumerator):
-        if denumerator==0:
-            return "Denumerator must be non-zero value"
-        numerator,denumerator=self.normalize(numerator,denumerator)
-        self.__numerator = numerator
-        self.__denumerator = denumerator
+        try:
+            if type(numerator)!=int:
+               raise RationalException("Must be integer",numerator)
+            if type(denumerator) != int:
+               raise RationalException("Must be integer", denumerator)
+            if denumerator<=0:
+               raise RationalException("Denumenator must be positive ", denumerator)
+            if numerator<=0:
+               raise RationalException("Numenator must be positive ", numerator)
+            else:
+                numerator,denumerator=self.normalize(numerator,denumerator)
+                self.__numerator = numerator
+                self.__denumerator = denumerator
+        except RationalException as error:
+            error.print_obj()
 
     def __repr__(self):
             return f"{self.__numerator},{self.__denumerator}"
+    @staticmethod
+    def gcd(number1, number2):
+        if number1 % number2 == 0:
+            return number2
+        if number2 % number1 == 0:
+            return number1
+        else:
+            x = 0
+            for i in range(1, number1):
+                if number1 % i == 0 and number2 % i == 0:
+                    x = i
+            return x
 
     def normalize(self,numerator,denumerator):
-        x = gcd(numerator,denumerator)
+        x = self.gcd(numerator,denumerator)
         return numerator//x, denumerator//x
 obj1 = Rational(10,5)
 print(obj1)
--- src/first_month/task_1_5_2.py
+++ src/first_month/task_1_5_2.py
@@ -1,40 +1,73 @@
 class MoneyException(Exception):
-    def __init__(self,message,amount):
-        self.__message=message
-        self.__amount=amount
-
+    def __init__(self, message, amount):
+        self.__message = message
+        self.__amount = amount
 
     def print_obj(self):
-        print(self.__message,self.__amount)
+        print(self.__message, self.__amount)
+
 
 class Money:
-    def __init__(self,am,currency):
-        self.__amount=am
-        self.__currency =currency
-
-    n = 100
-    try:
-        if n < 0:
-            raise MoneyException("Negative Value", n)
-    except MoneyException as me:
-        me.print_obj()
+    def __init__(self, am, currency):
+        try:
+            if type(am) != int:
+                raise MoneyException("Must be integer", am)
+            if type(currency) != str:
+                raise MoneyException("Must be string", currency)
+            if am < 0:
+                raise MoneyException("Negative Value", am)
+            else:
+                self.__amount = am
+                self.__currency = currency
+        except MoneyException as me:
+            me.print_obj()
 
     def __repr__(self):
         return f"{self.__amount},{self.__currency}"
-    def __add__(self,obj3):
-        sum = self.__amount+obj3.__amount
+
+    def __add__(self, obj3):
+        sum = self.__amount + obj3.__amount
         cur = self.__currency
-        return Money(sum,cur)
-    def __sub__(self,obj4):
-        sub = self.__amount-obj4.__amount
+        return Money(sum, cur)
+
+    def __sub__(self, obj4):
+        sub = self.__amount - obj4.__amount
         cur = self.__currency
-        if sub<0:
+        if sub < 0:
             return "amount can't be negative number"
         else:
-           return Money(sub,cur)
+            return Money(sub, cur)
+
+    def get_amount(self):
+        return self.__amount
+
+    def set_amount(self, other):
+        try:
+            if type(other) != int:
+                raise MoneyException("Must be integer", other)
+            if other < 0:
+                raise MoneyException("Negative Value", other)
+            else:
+                self.__amount = other
+        except MoneyException as me:
+            me.print_obj()
+
+    def get_currency(self):
+        return self.__currency
+
+    def set_currency(self, other):
+        try:
+            if type(other) != str:
+                raise MoneyException("Must be string", other)
+            else:
+                self.__currency = other
+        except MoneyException as me:
+            me.print_obj()
+
+
 obj1 = Money(-100, "USD")
 obj2 = Money(20, "USD")
-obj3 = obj1+obj2
-obj4 = obj1-obj2
+obj3 = obj1 + obj2
+obj4 = obj1 - obj2
 me.print_obj3()
 me.print_obj4()
