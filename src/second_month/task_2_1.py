4d54a52f7c8be2498c6a8775144119a387ca7803
Nare Torosyan <torosyan.nare@gmail.com>
2021-05-27 00:45:45 +0400

    changedalltasks

--- src/second_month/task_2_1.py
+++ src/second_month/task_2_1.py
@@ -2,12 +2,10 @@ import numpy as np
 #1 Write a NumPy program to convert a list of numeric values into a one-dimensional NumPy array.
 def convert(list):
     return np.array(list)
-print(convert([1,2,3]))
 
 #2 Write a NumPy program to create a NumPy array with values ranging from 2 to 10.
 def create(a,b):
     return np.arange(a,b)
-print(create(2,10))
 
 
 #3 Write a NumPy program to create a null vector of size 10 and update sixth to eight values to 11.
@@ -15,12 +13,12 @@ def update(a,b,c):
     arr=np.zeros(a)
     arr[b:c]=11
     return arr
-print(update(10,5,8))
+
 
 #4 Write a NumPy program to test whether each element of a 1-D array is also present in a second array.
 def check(arr1,arr2):
     return np.in1d(arr1,arr2)
-print(check(np.array([1,2,3,4,7,8]),np.array([4,7,1])))
+
 
 def main():
     print(convert([1,2,3]))
--- src/second_month/task_2_2.py
+++ src/second_month/task_2_2.py
@@ -1,41 +1,37 @@
 import numpy as np
 #1 Write a NumPy program to compute the multiplication of two given matrixes
-x1=np.array([[2,2,6],[1,9,10]])
-y1=np.array([[2,2],[1,4],[5,6]])
 def mul(x,y):
     return x.dot(y)
-print(mul(x1,y1))
 
 from numpy.linalg import det,inv
 #2 Write a NumPy program to compute the determinant of an array
-arr=np.array([[1,2,4],[3,4,5],[3,4,9]])
 def determinant(array):
-    return det(array)
-print(det(arr))
+    return np.linalg.det(array)
+
 
 #3 Write a NumPy program to compute the sum of the diagonal element of a given array
-arr=np.array([[1,2,4],[3,4,5],[3,4,9]])
 def sum_of_diagonal_elements(array):
     return np.trace(array)
-print(sum_of_diagonal_elements(arr))
+
 
 #4Write a NumPy program to compute the inverse of a given matrix
-arr=np.array([[1,2,4],[3,4,5],[3,4,9]])
 def inverse_matrix(array):
-    return inv(array)
-print(inverse_matrix(arr))
+    return np.linalg.inv(array)
+
 
 #5Write a NumPy program to generate matrix and write it to a file, then again read from file that matrix.
-arr=np.array([[1,2,4],[3,4,5],[3,4,9]])
+arr = np.arange(1, 12, 2).reshape(2, 3)
 def create_file(array):
     np.save("some_array",array)
     return np.load("some_array.npy")
 print(create_file(arr))
 
-
 def main():
-    print(mul(np.array([[2,2,6],[1,9,10]]),np.array([[2,2],[1,4],[5,6]])))
-    print(det(np.array([[1,2,4],[3,4,5],[3,4,9]])))
-    print(np.array([[1,2,4],[3,4,5],[3,4,9]]))
-    print(inverse_matrix(np.array([[1,2,4],[3,4,5],[3,4,9]])))
-    print(create_file(np.array([[1,2,4],[3,4,5],[3,4,9]])))
+    x1 = np.array([[2, 2, 6], [1, 9, 10]])
+    y1 = np.array([[2, 2], [1, 4], [5, 6]])
+print(mul(x1,y1))
+print(det(x1))
+print(sum_of_diagonal_elements(x1))
+print(inverse_matrix(x1))
+
+
