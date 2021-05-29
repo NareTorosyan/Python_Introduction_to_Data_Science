7d15ee5299462bb417a325190f6c4758debb31ac
Nare Torosyan <torosyan.nare@gmail.com>
2021-05-29 17:53:45 +0400

    changedtask2.3

--- src/second_month/task_2_2.py
+++ src/second_month/task_2_2.py
@@ -1,10 +1,10 @@
 import numpy as np
+from numpy.linalg import det
 #1 Write a NumPy program to compute the multiplication of two given matrixes
 def mul(x,y):
     return x.dot(y)
 
-from numpy.linalg import det,inv
-#2 Write a NumPy program to compute the determinant of an array
+
 def determinant(array):
     return np.linalg.det(array)
 
@@ -20,19 +20,19 @@ def inverse_matrix(array):
 
 
 #5Write a NumPy program to generate matrix and write it to a file, then again read from file that matrix.
-arr= np.random.randint(5, size=(2,4))
+
 def create_file(array):
     np.save("some_array",array)
     return np.load("some_array.npy")
-print(create_file(arr))
+
 
 def main():
+    arr = np.random.randint(5, size=(2, 4))
     x1 = np.array([[2, 2, 6], [1, 9, 10]])
     y1 = np.array([[2, 2], [1, 4], [5, 6]])
     print(mul(x1,y1))
-    print(det(x1))
     print(sum_of_diagonal_elements(x1))
-    print(inverse_matrix(x1))
+    print(create_file(arr))
 main()
 
 
