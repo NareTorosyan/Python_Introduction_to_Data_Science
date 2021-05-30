af043b70c9bcf7d32e623b23deb4aed29dfdf69a
Nare Torosyan <torosyan.nare@gmail.com>
2021-05-31 02:36:34 +0400

    addedtask2.4

--- src/second_month/task_2_4.py
+++ src/second_month/task_2_4.py
@@ -27,11 +27,6 @@ def main():
     client_data=np.array(["client1","client2","client3","client4","clinet5"])
     other_data = {"Sales":[100,200,700,500,900],"Feedback": [5,5,4,3,5],"Crosssale": ["Yes","Yes","No","Yes","Yes"]}
     other_data_dataframe =pd.DataFrame(other_data)
-    exam_data = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
-                 'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
-                 'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
-                 'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
-    exam_data_dataframe=pd.DataFrame(exam_data,index=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'])
     print(add_sub_mul_div(series1,series2))
     print(dic_to_series(sales_data))
     print(np_array_to_series(client_data))
