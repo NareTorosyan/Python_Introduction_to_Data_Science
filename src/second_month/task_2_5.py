6b9cc0b8b546043e86a010e949de115ea420410a
Nare Torosyan <torosyan.nare@gmail.com>
2021-06-19 13:26:30 +0400

    addeddatacleaner

--- src/second_month/task_2_5.py
+++ src/second_month/task_2_5.py
@@ -3,11 +3,7 @@ import pandas as pd
 import csv
 file = open(r'C:\Users\user\Desktop/data.csv')
 lines=file.readlines()
-for index, line in enumerate(lines):
-    print("Line {}: {}".format(index,line.strip()))
 def data_cleaner(list):
-    for i in range(0,len(list)):
-     line=list[i]
-     line=list.replace("+","")
+    for line in list:
+        Clean_row=line.replace("+","")
 print(data_cleaner(lines))
-
