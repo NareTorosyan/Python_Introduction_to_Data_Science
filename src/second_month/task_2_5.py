5e4054939de5ff6788d75d106b79257226de44ec
Nare Torosyan <torosyan.nare@gmail.com>
2021-06-20 15:22:07 +0400

    writeradded

--- src/second_month/task_2_5.py
+++ src/second_month/task_2_5.py
@@ -6,10 +6,8 @@ lines=file.readlines()
 for line in lines:
     clean_row = line.replace("+", "")
 
-with open(r'C:\Users\user\Desktop/new.csv',"a") as f:
+with open(r'C:\Users\user\Desktop/new.csv',"w") as f:
     writer=csv.writer(f)
-    new_lines = file.readlines()
-for line in new_lines:
     writer.writerow(clean_row)
 
 
