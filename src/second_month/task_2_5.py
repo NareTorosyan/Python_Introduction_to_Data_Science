b9e4e2d972352396540320fd5707ada15ff0297e
Nare Torosyan <torosyan.nare@gmail.com>
2021-06-20 14:28:12 +0400

    addedwriter

--- src/second_month/task_2_5.py
+++ src/second_month/task_2_5.py
@@ -3,7 +3,11 @@ import pandas as pd
 import csv
 file = open(r'C:\Users\user\Desktop/data.csv')
 lines=file.readlines()
-def data_cleaner(list):
-    for line in list:
-        Clean_row=line.replace("+","")
-print(data_cleaner(lines))
+for line in lines:
+    clean_row = line.replace("+", "")
+
+with open(r'C:\Users\user\Desktop/new.csv',"w") as f:
+    writer=csv.writer(f)
+    new_lines = file.readlines()
+    for line in lines:
+        writer.writerow(clean_row)
